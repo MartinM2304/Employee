b005603f3743ad48292e9150890286c11e2055ce
MartinM2304 <72139148+MartinM2304@users.noreply.github.com>
2021-01-26 23:11:27 +0200

    Step 2

---
+++ Main.java
@@ -0,0 +1,28 @@
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class Main {
+public static void main(String[] args) {
+		
+		
+		//JPanel panel = new JPanel();
+		//JTextField text = new JTextField();
+		
+		SwingUtilities.invokeLater(new Runnable () {
+			public void run (){		
+		JFrame frame = new MainFrame("Company personal");		
+		frame.setVisible(true);
+		frame.setSize(700, 850);
+		//frame.pack();
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+			}
+		});
+		
+		
+
+	}
+
+}
